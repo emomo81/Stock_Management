3222ccb4f20e13f5f68b40d58758b5d46b918670
emomo81 <emmanuelmomo81@gmail.com>
2026-01-27 06:08:18 +0200

    Improve users management screen

--- screens/Users.tsx
+++ screens/Users.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import { API_URL } from '../config';
+
 const Users: React.FC = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
